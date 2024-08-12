93cc728ca25a103ca60c161699c21e0bf47a364b
João Leme <joao.leme2003@gmail.com>
2024-08-12 20:37:20 -0300

    Design Inicial

--- app/src/main/java/com/example/combusapp/MainActivity.kt
+++ app/src/main/java/com/example/combusapp/MainActivity.kt
@@ -3,43 +3,107 @@ package com.example.combusapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.combusapp.ui.theme.CombusAppTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+
+    val customFontFamily = FontFamily(
+        Font(R.font.worksans_normal, FontWeight.Normal),
+        Font(R.font.worksans_bold, FontWeight.Bold),
+        Font(R.font.worksans_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.worksans_medium, FontWeight.Medium)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CombusAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            CombusAppInicial(customFontFamily)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CombusAppInicial(customFontFamily: FontFamily) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Bem-vindo ao Combustível Inteligente",
+            fontFamily = customFontFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = "Como podemos te ajudar?",
+            style = MaterialTheme.typography.bodyLarge,
+            fontFamily = customFontFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        FuelOption("Consumo do carro", Icons.Default.AccountCircle, customFontFamily)
+        FuelOption("Preço da viagem", Icons.Default.AccountBox, customFontFamily)
+        FuelOption("Qual combustível abastecer?", Icons.Default.ExitToApp, customFontFamily)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CombusAppTheme {
-        Greeting("Android")
+fun FuelOption(title: String, icon: ImageVector, customFontFamily: FontFamily) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(imageVector = icon, contentDescription = null)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = title,
+            fontFamily = customFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = MaterialTheme.typography.bodyLarge.fontSize
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFuellyApp() {
+    val customFontFamily = FontFamily(
+        Font(R.font.worksans_normal, FontWeight.Normal),
+        Font(R.font.worksans_bold, FontWeight.Bold),
+        Font(R.font.worksans_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.worksans_medium, FontWeight.Medium)
+    )
+    Surface {
+        CombusAppInicial(customFontFamily)
     }
 }
